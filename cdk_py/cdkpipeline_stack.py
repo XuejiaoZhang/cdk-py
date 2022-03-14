498c96fc880c2bce684523aef67693993b4cb9fa
zxj <aa@aa.com>
2022-03-14 17:00:20 +0100

    cdk synth branch name read from sqs

--- cdk_py/cdkpipeline_stack.py
+++ cdk_py/cdkpipeline_stack.py
@@ -320,8 +320,8 @@ class CDKPipelineStack(core.Stack):
                         resources=["*"],
                     )
                 ],
-                build_command="echo $BRANCH; cdk list -c branch_name=$BRANCH",
-                synth_command="echo $BRANCH; cdk synth -c branch_name=$BRANCH",
+                build_command="echo $BRANCH; BRANCH=$(python scripts/get_branch_name_from_sqs_not_delete.py); echo $BRANCH; cdk list -c branch_name=$BRANCH",
+                synth_command="echo $BRANCH; BRANCH=$(python scripts/get_branch_name_from_sqs_not_delete.py); echo $BRANCH; cdk synth -c branch_name=$BRANCH",
 
                 # build_command="BRANCH=$(python scripts/get_branch_name_from_ssm.py); echo $BRANCH; cdk list -c branch_name=$BRANCH",
                 # synth_command="BRANCH=$(python scripts/get_branch_name_from_ssm.py); echo $BRANCH; cdk synth -c branch_name=$BRANCH",
---
+++ scripts/get_branch_name_from_sqs_not_delete.py
@@ -0,0 +1,51 @@
+import boto3
+import os
+
+sqs_client = boto3.client('sqs')
+
+#CODEBUILD_INITIATOR=codepipeline/featurebranchpipelinewebhookReadyForFeatureBranchPipeline
+
+def branch_name_check(branch_name, branch_prefix):
+    if branch_name.startswith(branch_prefix):
+        return True
+    else:
+        return False
+
+# branch_name = 'generator2'
+branch_prefix = 'feature-branch-pipeline-'
+
+sqs_url = os.getenv('SQS_URL')
+if sqs_url:
+	# Receive message from SQS queue
+	response = sqs_client.receive_message(
+	    QueueUrl=sqs_url,
+	    # AttributeNames=[
+	    #     'SentTimestamp'
+	    # ],
+	    # MaxNumberOfMessages=1,
+	    # MessageAttributeNames=[
+	    #     'All'
+	    # ],
+	    # VisibilityTimeout=0,
+	    # WaitTimeSeconds=0
+	)
+	# print(response)
+	if response.get('Messages', []):
+		message = response['Messages'][0]
+		receipt_handle = message['ReceiptHandle']
+
+		# Delete received message from queue
+		# sqs_client.delete_message(
+		#     QueueUrl=sqs_url,
+		#     ReceiptHandle=receipt_handle
+		# )
+		# print('Received and deleted message: %s' % message)
+		branch_name = message.get('Body', '')
+		# print(branch_name)
+
+		if branch_name_check(branch_name, branch_prefix):
+			print(branch_name)
+
+
+
+
