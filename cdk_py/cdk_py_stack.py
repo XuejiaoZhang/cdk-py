14861fdebbed1097dced19098dd465019a80f17d
zxj <aa@aa.com>
2022-03-15 19:40:34 +0100

    cleanup

--- cdk_py/cdk_py_stack.py
+++ cdk_py/cdk_py_stack.py
@@ -109,7 +109,6 @@ class CdkPyStack(core.Stack):
                 commands=[
                     "pip install -r requirements.txt",
                     "pip install -r requirements_dev.txt",
-                    "cat a.txt"
                     #"pytest --cov=infrastructure --cov-branch --cov-report term-missing -vvvv -s tests", #TODO
                     # "pytest --cov=dags --cov-branch term-missing -vvvv -s tests", #TODO
                    # "pytest --cov=dags --cov-branch --cov-report term-missing -vvvv -s tests", #TODO
